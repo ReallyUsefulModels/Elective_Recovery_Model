ab1b780758cf91d83633c54f23eeb58cf793b002
Stammers <Matthew.Stammers@uhs.nhs.uk>
2022-09-20 17:52:12 +0100

    part working version

--- ERM_ASDM_app.py
+++ ERM_ASDM_app.py
@@ -2,7 +2,6 @@ from ASDM.ASDM.Engine import Structure
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
-pd.options.display.max_columns = 100
 
 model = Structure(from_xmile='Elective Recovery Model.stmx')
 
@@ -10,26 +9,32 @@ model = Structure(from_xmile='Elective Recovery Model.stmx')
 
 st.title('Elective Recovery Model')
 
-model.clear_last_run()
-
 # Variables
-st.subheader('Slide the Slider to Vary The Percent increase in diagnostic capacity post COVID')
-diagnostic_capacity_increase = st.slider("Percent increase in diagnostic capacity post COVID", 1, 100, 1)
+st.subheader('Slide the Slider to Vary The starting proportion of patients waiting over 24mths for treatment')
+long_wait = st.slider("Waiting over 24mths for treatment", 1, 1000, 1)
+
+st.subheader('Number of Weeks to Run the Model For')
+sim_time = st.slider("Simulation Time", 1, 500, 1)
 
 # Edit Stock
-# model.add_stock(name='Percent increase in diagnostic capacity post COVID', equation={'nosubscript': str(diagnostic_capacity_increase)}, x=0, y=0, non_negative=True)
-model.add_aux(name='Percent increase in diagnostic capacity post COVID', equation={'nosubscript': str(diagnostic_capacity_increase)})
+model.add_stock(name='Waiting over 24mths for treatment', equation={'nosubscript': str(long_wait)}, x=0, y=0, non_negative=True)
+# model.add_aux(name='Average pre COVID wait for diagnostics', equation={'nosubscript': str(per_urg_less_six)})
 
 # Run Model
-model.simulate()
+model.clear_last_run()
+model.simulate(simulation_time=sim_time, dt=0.25)
 
 # Create the Model
 
 df_outcome = model.export_simulation_result()
 
-df_backlog = df_outcome[['Total waiting for diagnostics or treatment','Waiting 6mths for treatment','Waiting 6 to 12mths for treatment','Waiting 12 to 24mths for treatment','Waiting over 24mths for treatment']]
+df_demand = df_outcome[['Routine treatment', 'Urgent treatment']]
+df_backlog = df_outcome[['Total waiting for diagnostics or treatment','Waiting 6 to 12mths for treatment','Waiting 12 to 24mths for treatment','Waiting over 24mths for treatment']]
 
 # Then plot
 
-st.subheader('Number Waiting By Month')
+st.subheader('Estimated Backlog in Weeks')
 st.line_chart(df_backlog)
+
+st.subheader('Demand for Urgent and Routine Treatment')
+st.line_chart(df_demand)
