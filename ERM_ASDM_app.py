3c90f79166c7c47f4ba87aa3c0e2caf4a21d30bb
Matt Stammers <matthew.stammers@uhs.nhs.uk>
2023-07-16 23:21:46 +0100

    partially working streamlit model

--- ERM_ASDM_app.py
+++ ERM_ASDM_app.py
@@ -1,60 +1,70 @@
-from ASDM.ASDM.Engine import Structure
+from ASDM.ASDM import Structure
+from ASDM.Utilities import plot_time_series
+import pandas as pd
+import plotly.express as px
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 import time
 
-model = Structure(from_xmile='Elective Recovery Model.stmx')
+class PatientFlow(Structure):
+    def __init__(self):
+        super(PatientFlow, self).__init__()
+        self.add_stock("Waiting_list", 100, in_flows=['GP_referrals'], out_flows=['Hospital_admission_rate'])
+        self.add_stock("In_hospital", 30, in_flows=['Hospital_admission_rate'], out_flows=['Treatment_completion_rate'])
+        self.add_stock("In_community_care", 100, in_flows=['Treatment_completion_rate'], out_flows=['Community_care_leaving_rate'])
+        
+        self.add_aux("Hospital_bed_capacity", 30)
+        self.add_aux("Hospital_bed_vacancy", "Hospital_bed_capacity-In_hospital")
+        self.add_aux("p_completion", 0.1)
+        self.add_aux("p_leaving", 0.1)
 
-# Name the app
+        self.add_flow("GP_referrals", 3)
+        self.add_flow(
+            "Hospital_admission_rate", 
+            "MIN(Waiting_list, MAX(Treatment_completion_rate, Hospital_bed_vacancy))",  # mimicing the non-negative stock behaviour
+            )
+        self.add_flow(
+            "Treatment_completion_rate", 
+            "RBINOM(In_hospital, p_completion)", 
+            )
+        self.add_flow(
+            "Community_care_leaving_rate", 
+            "RBINOM(In_community_care, p_leaving)", 
+            )
 
+        # mapping SD functions to Python functions
+        self.custom_functions['MAX'] = max
+        self.custom_functions['MIN'] = min
+
+
+# Streamlit App
 st.title('Elective Recovery Model')
-
-# Variables
 st.subheader('Slide the Sliders to the left to Vary The starting proportion of patients waiting over 24mths for treatment and the time to simulate over in weeks')
 
-# Add a slider to the sidebar:
-sim_time = st.sidebar.slider(
-    'Number of Weeks to Run the model for',
-    1, 500, 250
-)
+# Set up your sliders
+sim_time = st.sidebar.slider('Number of Weeks to Run the model for', 1, 500, 250)
+long_wait = st.sidebar.slider('Waiting over 24mths for treatment at model start', 1, 1000, 150)
+shorter_wait = st.sidebar.slider('Waiting 6 to 12mths for treatment at model start', 1, 1000, 400)
 
-# Add a slider to the sidebar:
-long_wait = st.sidebar.slider(
-    'Waiting over 24mths for treatment at model start',
-    1, 1000, 150
-)
-
-# Add a slider to the sidebar:
-shorter_wait = st.sidebar.slider(
-    'Waiting 6 to 12mths for treatment at model start',
-    1, 1000, 400
-)
-
-# Edit Stock
-model.add_stock(name='Waiting over 24mths for treatment', equation={'nosubscript': str(long_wait)}, x=0, y=0, non_negative=True)
-model.add_stock(name='Waiting 6 to 12mths for treatment', equation={'nosubscript': str(shorter_wait)}, x=0, y=0, non_negative=True)
-
-
-# Run Model
+# Model Object Created
+model = PatientFlow()
 model.clear_last_run()
-model.simulate(simulation_time=sim_time, dt=0.25)
+model.simulate(time=20, dt=1)
 
-# Create the Model
+# Extract the simulation results
+results = model.export_simulation_result()
+df_outcome = pd.DataFrame(results)
 
-df_outcome = model.export_simulation_result()
+df_demand = df_outcome[['In_hospital', 'In_community_care']]
+df_backlog = df_outcome[['Waiting_list']]
 
-df_demand = df_outcome[['Routine treatment', 'Urgent treatment']]
-df_backlog = df_outcome[['Total waiting for diagnostics or treatment','Waiting 6 to 12mths for treatment','Waiting 12 to 24mths for treatment','Waiting over 24mths for treatment']]
-
-# Then plot
-
+# Plot the results
 st.subheader('Estimated Backlog in Weeks')
 st.line_chart(df_backlog)
 
 if st.checkbox('Show Backlog Dataframe'):
-    chart_data = df_backlog
-    chart_data
+    st.write(df_backlog)
 
 'Calculating Demand...'
 
@@ -63,10 +73,9 @@ latest_iteration = st.empty()
 bar = st.progress(0)
 
 for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Re-Running the Engine {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+    latest_iteration.text(f'Re-Running the Engine {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
 
 '...done!'
 
@@ -74,5 +83,4 @@ st.subheader('Demand for Urgent and Routine Treatment')
 st.line_chart(df_demand)
 
 if st.checkbox('Show Treatment Demand DataFrame'):
-    chart_data2 = df_demand
-    chart_data2
+    st.write(df_demand)
--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # Elective_Recovery_Model
 This Model looks at Covid-19 Recovery in the NHS
 
-All 3 major python frameworks have been used to make this model
-
-It is still in development but is starting to get there. If anyone would like to submit an issue or pull request feel free to do so.
-
-The model structure is as below. As you can see it is more complex than the smoking cessation model so if you are new to system dynamics I strongly recommend you start there.
+The model structure is as below:
 
 ![Elective_Recovery_Model](https://github.com/ReallyUsefulModels/Elective_Recovery_Model/blob/main/Elective_Recovery_Model.PNG)
 
 To see the working model please go to: https://reallyusefulmodels-elective-recovery-model-erm-asdm-app-kqu8vf.streamlitapp.com/
+
+ASDM has now been released as an official PyPi Framework: https://pypi.org/project/asdm/
+
+It can be installed with 
+
+```bat
+pip install asdm
+```
+
+However, we recommed setting up this notebook and script using pipenv
+
+```bat 
+pip install pipenv
+```
+
+Followed by 
+
+```bat
+pipenv install
+```
+
+Then you should be good to run the notebook in either VS code, jupyterlab or even streamlit - whichever is preferred
