32fb59aab5edac987fe946e8b084f01c0c2c68cd
Matt Stammers <matthew.stammers@uhs.nhs.uk>
2023-07-22 19:04:28 +0100

    explainers and log toggler added

--- ERM_ASDM_app.py
+++ ERM_ASDM_app.py
@@ -52,7 +52,8 @@ model = load_model('models/Elective Recovery Model.stmx')
 # Check if the model is successfully loaded
 if model is not None:
     # Add Streamlit components for user inputs (e.g., sliders, number input)
-    weeks_to_simulate = st.slider("Select the number of weeks to simulate:", min_value=1, max_value=104, value=52)
+    weeks_to_simulate = st.slider("Select the number of weeks to simulate:", min_value=1, max_value=52, value=20)
+    use_log_scale = st.checkbox("Use Logarithmic Scale", value=True)
 
     # Run the simulation and get the results
     x_values, y_values = run_simulation(model, weeks_to_simulate)
@@ -65,11 +66,17 @@ if model is not None:
     # Update the plot layout
     fig.update_layout(title_text='Elective Recovery in the NHS',
                       xaxis_title='Weeks',
-                      yaxis_title='Number of Patients (log scale)',  # Update the y-axis label
+                      yaxis_title='Number of Patients (log scale)' if use_log_scale else 'Number of Patients',
                       xaxis_type='linear',  # Set the x-axis scale to linear
-                      yaxis_type='log',  # Set the y-axis scale to logarithmic
+                      yaxis_type='log' if use_log_scale else 'linear',  # Toggle y-axis scale based on checkbox
                       legend=dict(orientation='h', x=0.5, y=-0.15),  # Move legend to the bottom
                       margin=dict(l=50, r=50, t=50, b=50))  # Add margins for better display
 
+    # Display explanatory text before the chart
+    st.markdown("The chart below displays the number of patients waiting for different elective treatments over time. The vertical axis is in logarithmic scale to better visualize the changes in patient numbers for different waiting time categories.")
+    
     # Display the plot
     st.plotly_chart(fig)
+
+    # Display explanatory text after the chart
+    st.markdown("The chart shows how the number of patients in various waiting time categories evolves over the selected number of weeks. It provides insights into the recovery of elective services in the NHS and helps identify areas that may require attention for improvement.")
